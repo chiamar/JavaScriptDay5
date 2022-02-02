84af93c2585cf23c84ce5a73c8135793c80317b6
Petra Christina <p.sedlaczek@drei.at>
2022-02-02 14:08:12 +0100

    Finish Intermediate exercise

--- Intermediate/intermediate.js
+++ Intermediate/intermediate.js
@@ -31,7 +31,6 @@ class Motorbike extends Car {
 
     }
     message() {
-        // return `My ${this.vehicle} has the following attributes: ${this.brand} ${this.type}, ${this.wheels} wheels, in ${this.color}, with hp ${this.hp} and it has the following extras: ${this.accessories.join(", ")}.<br>`;
         return `${super.message()}  Furthermore, it has ${this.wheels} wheels!`;
     }
 }
@@ -42,10 +41,10 @@ var ford = new Car("Ford", "Galaxy", "dark blue", 190, ["acc", "radio", "mp3-pla
 var tesla = new Car("Tesla", "Model S", "silver", 1020, ["acc", "auto-pilot", "bord-computer", "and everything else"], "car");
 var kawasaki = new Motorbike("Kawasaki", "Z900", "green", 150, ["ergonomic design", "abs", " connection", "cupholder"], "motorbike", 2)
 
-document.getElementById("car").innerHTML += skoda.message();
-document.getElementById("car").innerHTML += ford.message();
-document.getElementById("car").innerHTML += tesla.message();
-document.getElementById("car").innerHTML += kawasaki.message();
+document.getElementById("vehicle").innerHTML += skoda.message();
+document.getElementById("vehicle").innerHTML += ford.message();
+document.getElementById("vehicle").innerHTML += tesla.message();
+document.getElementById("vehicle").innerHTML += kawasaki.message();
 
 
 class Animal {
@@ -66,18 +65,34 @@ class Animal {
     }
 
     message() {
-        return `Our lovely ${this.name} is a ${this.species}, ${this.pronoun} is ${this.age} old and is a family member of ${this.family.join(", ")} and is currently living in ${this.home}.<br>`;
+        return `<br>Our lovely ${this.name} is a ${this.species}, ${this.pronoun} is ${this.age} old and is a family member of ${this.family.join(", ")} and is currently living in ${this.home}`;
+    }
+}
+
+// Extend Class Animal
+class Fish extends Animal {
+    habitat;
+
+
+    constructor(name, species, age, family, home, pronoun, habitat) {
+        super(name, species, age, family, home, pronoun);
+        this.habitat = habitat;
+
+    }
+    message() {
+        return `${super.message()} in ${this.habitat}!`;
     }
 }
 
 var fulong = new Animal("Fu Long", "Panda", 14, ["Yang Yang", "Long Hui", "Fu Bao", "Fu Hu"], "China", "he");
 var monty = new Animal("Monty", "dog", 1.5, ["Christian", "Lisa"], "Vienna, Austria", "he");
 var happy = new Animal("Happy", "cat", 5, ["Nala", "Beate", "Rudolf"], "Vienna, Austria", "she");
+var nemo = new Fish("Nemo", "clownfish", 3, ["Marlin", "Dory"], "Great Barrier Reef", "he", "saltwater");
 
 document.getElementById("animal").innerHTML += fulong.message();
 document.getElementById("animal").innerHTML += monty.message();
 document.getElementById("animal").innerHTML += happy.message();
-
+document.getElementById("animal").innerHTML += nemo.message();
 
 class Person {
     firstName;
@@ -99,14 +114,32 @@ class Person {
     }
 
     message() {
-        return ` ${this.firstName} ${this.lastName} is ${this.age} old and has ${this.codingSkills} coding skills. ${this.pronoun} like(s) to ${this.hobbies.join(", to ")} and currently lives in ${this.home}.<br>`
+        return `<br>${this.firstName} ${this.lastName} is ${this.age} old and has ${this.codingSkills} coding skills. ${this.pronoun} like(s) to ${this.hobbies.join(", to ")} and currently lives in ${this.home}`
     }
 }
+
+// Extend Class Person
+class Profession extends Person {
+    jobposition;
+
+
+    constructor(firstName, lastName, age, codingSkills, hobbies, home, pronoun, jobposition) {
+        super(firstName, lastName, age, codingSkills, hobbies, home, pronoun);
+        this.jobposition = jobposition;
+
+    }
+    message() {
+        return `${super.message()} and holds the job position of ${this.jobposition}!`;
+    }
+}
+
 
 var petra = new Person("Petra", "Sedlaczek", 35, "advanced", ["code", "learn languages", "read", "run"], "Vienna, Austria", "She");
 var stephan = new Person("Stephan", "Reindl", 33, "intermediate", ["code", "play music", "read", "run"], "Vienna, Austria", "He");
 var pascal = new Person("Pascal", "Doe", 29, "expert", ["code", "code", "code", "party"], "Paris, France", "They");
+var lisa = new Profession("Lisa", "Duschek", 30, "expert", ["code", "protect data", "found a startup", "network", "volunteer"], "Vienna, Austria", "She", "CEO");
 
 document.getElementById("person").innerHTML += petra.message();
 document.getElementById("person").innerHTML += stephan.message();
 document.getElementById("person").innerHTML += pascal.message();
+document.getElementById("person").innerHTML += lisa.message();
